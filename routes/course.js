2ba3c982f6b509b0fadbc88b4b1b1fc5516e93f7
Sam Orion <samorion@Sams-MacBook-Air.local>
2025-10-31 21:53:12 +0530

    Added Express router routes

---
+++ routes/course.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { Router } = express;
+
+const courseRouter = Router();
+
+courseRouter.post("/purchase", function (req, res) {
+  res.json({
+    message: "course purchase endpoint",
+  });
+});
+
+courseRouter.get("/preview", function (req, res) {
+  res.json({
+    message: "courses endpoint",
+  });
+});
+
+module.exports = {
+  courseRouter: courseRouter,
+};
