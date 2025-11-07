dd9cd5d9b46eafdcdf45e1ce1f2e8eb17cd47dc5
Sam Orion <samorion@Sams-MacBook-Air.local>
2025-11-08 00:55:01 +0530

    Added final endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { Router } = express;
-const { userModel, purchaseModel } = require("../db");
+const { userModel, purchaseModel, courseModel } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_USER_PASSWORD } = require("../config");
+const { userMiddleware } = require("../middleware/user");
 
 const userRouter = Router();
 
@@ -55,15 +56,25 @@ userRouter.post("/signin", async function (req, res) {
   }
 });
 
-userRouter.get("/purchase", async function (req, res) {
+userRouter.get("/purchase", userMiddleware, async function (req, res) {
   const userId = req.userId;
 
   const purchases = await purchaseModel.find({
     userId,
   });
 
+  let purchasedCourseIds = [];
+  for (let i = 0; i < purchases.length; i++) {
+    purchasedCourseIds.push(purchases[i].courseId);
+  }
+
+  const coursesData = await courseModel.find({
+    _id: { $in: purchasedCourseIds },
+  });
+
   res.json({
     purchases,
+    coursesData,
   });
 });
 
